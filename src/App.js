6e8efca0bb679457efabe59ac3212419e2734ae9
Cristian Calderon <cdcalderonrojas@gmail.com>
2022-11-28 17:22:04 +0100

    Contando y buscando TODOs

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 
 // crear una lista falsa
-const Defaulttodos = [
+const DefaultTodos = [
   { text: "Cortar cebolla", completed: false },
   { text: "Tomar el curso Intro a React", completed: false },
   { text: "Llorar con la Lloronas", completed: false },
@@ -17,12 +17,24 @@ const Defaulttodos = [
 
 // funcion que comienza con una letra mayuscula es un componente.
 function App() {
-  const [todos, setTodo] = React.useState(Defaulttodos)
+  const [todos, setTodos] = React.useState(DefaultTodos)
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length
   const totalTodos = todos.length;
 
+  // Esta vacio 
+  let searchedTodos = []
+  
+  if(!searchValue.length >= 1){
+    searchedTodos = todos;
+  } else{
+    searchedTodos = todos.filter(todo =>{
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText)
+    })
+  }
 
   return (
     // React nos indica que puede recibir solo 1 elemento de html para renderizarlo.
@@ -39,7 +51,7 @@ function App() {
       />
 
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodos.map((todo) => (
           // Cuando hacemos render de una lista en este caso todo's-items, tenemos que enviarle una propiedad especial que se llama "key" a nuestros componentes para que React pueda identificar cual componente es cual dentro de una lista.  
           <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
         ))}
